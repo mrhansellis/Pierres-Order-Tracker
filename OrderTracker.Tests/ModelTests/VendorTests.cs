a99afbc71be18dc65c8e6a7c59698ae1164afdf8
Hans Ellis <bullhorn@gmail.com>
2022-12-09 12:20:47 -0800

    pass vendorname get test

--- OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -11,8 +11,19 @@ namespace ToDoList.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor();
+      Vendor newVendor = new Vendor("test");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+    [TestMethod]
+    public void VendorConstructor_ReturnsVendorName_String()
+    {
+      //Arrange
+      string name = "test name";
+      Vendor newVendor = new Vendor(name);
+      //Act
+      string result = newVendor.VendorName;
+      //Assert
+      Assert.AreEqual(name, result);
     }
   }
 }
